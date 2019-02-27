b19a5a73e1b2db05ce7ebff4805072eed2538a45
RAGAdox <rishitech882@gmail.com>
2019-02-28 02:09:12 +0530

    Basic React app with two component

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Building my first React Application</h2>
+        <button>Click me</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import ReactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
+const element = <h1>Hii This is Rishi Mukherjee</h1>;
+//console.log(element);
+ReactDom.render(element, document.getElementById("root"));
+ReactDom.render(<Counter />, document.getElementById("new"));
